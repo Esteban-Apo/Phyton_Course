83137203cb990bbe86d62a685a899739589c1227
estebanmorales <estebanmoralesmarin025@gmail.com>
2025-07-18 21:45:55 -0500

    Avances respecto al curso de python

---
+++ 01_Tipos_datos/tipos_datos.py
@@ -0,0 +1,19 @@
+#Cadenas de texto de una sola linea
+'string' 
+"string"
+
+"""string"""
+
+#Cadenas de texto de multiples lineas 
+''''string 
+    esteban 
+        morales 
+            marin 
+                SI''' 
+
+
+#Tipos de datos con la función Type 
+print(type(5)) #Type int 
+print(type(1.5)) #Type float
+print(type(3 + 5j)) #Type complex
+print(type(True)) #Type boolean
--- 02_Variables/variables.py
+++ 02_Variables/variables.py
@@ -1,3 +1,8 @@
+#Variables (Forma Correcta de escribir variables)
+    #My-Variable = "" Forma incorrecta de escribir variables
+my_variable = True #Forma correcta de escribir variables 
+
+
 #Variables Numericas 
 a = 5
 b = 10
@@ -5,7 +10,7 @@ c = a + b
 
 d = 11
 d += 11
-print(d) # valor = 22, c = 11 + c 
+print(d) # valor = 22, d = 11 + d 
 print(c) #print = Imprimiar datos en pantalla (Terminal)
 
 
@@ -15,15 +20,22 @@ print(c) #print = Imprimiar datos en pantalla (Terminal)
 nombre = "Esteban" #En phyton se pueden imprimir en pantalla numeros, cadenas de texto, valores Booleanos, etc, etc
 print(nombre)
 
+
 #Concatenación no recomendada = """
 nombre = 'Laura'
 bienvenida = 'Hola ' + nombre + ' ¿Como estas?'
 print(bienvenida)
 
+
+
 #Concatenación en base a F strings 
 nombre ='Isabel'
 welcome = f'Hola {nombre}, ¿Como estas?'
 print(welcome)
+
+
+
+
 
 
 
